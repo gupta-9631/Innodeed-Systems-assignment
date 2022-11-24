b4a6a07a675b3efc3fc80609acb6e11d052ec872
Rahul <gupta-9631>
2022-11-24 14:45:41 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import Slider from "./component/Slider";
 import "./App.css";
+import Cars from "./api/cars.json";
+import CarPage from "./components/CarPage";
+import { useRef, useState } from "react";
 
 function App() {
+  let scrl = useRef(null);
+  const [scrollX, setscrollX] = useState(0);
+  const slide = (shift) => {
+    scrl.current.scrollLeft += shift;
+    setscrollX(scrollX + shift);
+  };
+
+  const scrollCheck = () => {
+    setscrollX(scrl.current.scrollLeft);
+  };
   return (
-    <div className="App">
-      <Slider />
-    </div>
+    <>
+      <div className="app" ref={scrl} onScroll={scrollCheck}>
+        {Cars.map((item) => (
+          <CarPage
+            key={item.id}
+            type={item.bodyType}
+            name={item.modelName}
+            modelType={item.modelType}
+            carImage={item.imageUrl}
+          />
+        ))}
+      </div>
+      <div className="app-btn_container">
+        <button className="left-btn button" onClick={() => slide(-400)}>
+          <img src="./images/chevron-circled.svg" alt="Right Button" />
+        </button>
+        <button className="right-btn button" onClick={() => slide(+400)}>
+          <img src="./images/chevron-circled.svg" alt="Left Button" />
+        </button>
+      </div>
+    </>
   );
 }
 
